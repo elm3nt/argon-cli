2223f02aa5519cf14a30f6f27fb9c51087a4109a
Deepak Adhikari <deeps.adhi@gmail.com>
2019-06-19 10:09:04 -0500

    Add sample C programs

---
+++ examples/code_password.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    char password[20];
+    int activation_code = 2019;
+    char secret[9] = "password";
+
+    if (argc != 2) {
+        printf("Usage: code_password <activation code>\n");
+
+        return -1;
+    }
+
+    int code = atoi(argv[1]);
+
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    if (code == activation_code) {
+        if (strcmp(secret, password) == 0) {
+            printf("Welcome!\n");
+        } else {
+            printf("Try again!\n");
+        }
+    } else {
+        printf("Try again!\n");
+    }
+
+    return 0;
+}
---
+++ examples/password.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    char password[20];
+    char secret[9] = "password";
+
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    if (strcmp(secret, password) == 0) {
+        printf("Welcome!\n");
+    } else {
+        printf("Try again!\n");
+    }
+
+    return 0;
+}
