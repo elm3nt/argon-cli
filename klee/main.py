450e3482b93e16227e91a0d6b91a71ac0d4f9d1f
Justin Nguyen <thienx99@gmail.com>
2019-06-19 13:51:16 -0500

    Updated argparse

--- klee/main.py
+++ klee/main.py
@@ -18,39 +18,42 @@ FILE_NAME = {
 
 
 def compile(test_path, c_file_path, c_file_name, bytecode_file_path):
-         compile = CMD['compile'].format(c_file_path, bytecode_file_path)
+        compile = CMD['compile'].format(c_file_path, bytecode_file_path)
 
-         print(compile)
-         os.system(CMD['bash'].format(compile))
+        print(compile)
+        os.system(CMD['bash'].format(compile))
 
 
 def test(test_path, c_file_name, bytecode_file_path):
-         test_result_file = FILE_NAME['result'].format(c_file_name)
-         test_result_path = os.path.join(test_path, test_result_file)
-         test_output_dir = os.path.join(test_path, c_file_name)
-         test = CMD['test'].format(test_output_dir, bytecode_file_path, klee_args, test_result_path)
-         print(test)
-         os.system(CMD['bash'].format(test))
+        test_result_file = FILE_NAME['result'].format(c_file_name)
+        test_result_path = os.path.join(test_path, test_result_file)
+        test_output_dir = os.path.join(test_path, c_file_name)
+        test = CMD['test'].format(test_output_dir, bytecode_file_path, klee_args, test_result_path)
+        os.system(CMD['bash'].format(test))
 
+def clean_up(input_dir):
+    dir_path_list = Path(input_dir).glob('**/*.bc')
+    for file in dir_path_list:
+        os.remove(str(file))
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('Usage: batch [test directory] [klee symbolic input args]')
         sys.exit(1)
 
-    test_dir = sys.argv[1]
+    input_dir = sys.argv[1]
     klee_args = sys.argv[2]
 
-    test_dir_path_list = Path(test_dir).glob('**/*.c')
+    test_dir_path_list = Path(input_dir).glob('**/*.c')
     for path in test_dir_path_list:
-         c_file_path = str(path.absolute())
-         c_file = os.path.basename(c_file_path)
-         c_file_name, _ = os.path.splitext(c_file)
-         test_path = os.path.dirname(c_file_path)
-         bytecode_file = FILE_NAME['bytecode'].format(c_file_name)
-         bytecode_file_path = os.path.join(test_path, bytecode_file)
+        c_file_path = str(path.absolute())
+        c_file = os.path.basename(c_file_path)
+        c_file_name, _ = os.path.splitext(c_file)
+        test_path = os.path.dirname(c_file_path)
+        bytecode_file = FILE_NAME['bytecode'].format(c_file_name)
+        bytecode_file_path = os.path.join(test_path, bytecode_file)
+        compile(test_path, c_file_path, c_file_name, bytecode_file_path)
 
-         compile(test_path, c_file_path, c_file_name, bytecode_file_path)
-
-         test(test_path, c_file_name, bytecode_file_path)
-
+        test(test_path, c_file_name, bytecode_file_path)
+        print(test_path)
+    clean_up(input_dir)
