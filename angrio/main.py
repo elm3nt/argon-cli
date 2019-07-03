d13215445668f2632349ce615cd2d826f8c2605a
Deepak Adhikari <deeps.adhi@gmail.com>
2019-07-03 15:14:45 -0500

    Add code to verify credentials

--- angrio/main.py
+++ angrio/main.py
@@ -4,10 +4,11 @@ import claripy
 from datetime import datetime
 
 from utils import file
+from utils import lists
 from core.const import *
 
 
-def symbolic_execution(input_file_path, output_dir_path, stdin):
+def symbolic_execution(input_file_path, output_dir_path, stdin, options, credentials):
     start_time = datetime.now()
 
     project = angr.Project(input_file_path)
@@ -30,6 +31,11 @@ def symbolic_execution(input_file_path, output_dir_path, stdin):
     if hasattr(simulation_manager, 'deadended'):
         index = 0
 
+        codes = ''
+        passwords = ''
+        generated_codes = []
+        generated_passwords = []
+
         for deadended in simulation_manager.deadended:
             content = ''
             test_file_name = FILE_NAME['angr-test'].format(index = str(index))
@@ -37,19 +43,24 @@ def symbolic_execution(input_file_path, output_dir_path, stdin):
 
             content += 'Arg(s)\n'
             for arg in args:
-                argv = deadended.solver.eval(arg, cast_to = bytes)
-                content += str(argv) + '\n'
-                print('arg: ', argv)
+                code = str(deadended.solver.eval(arg, cast_to = bytes))
+                print('arg: ', code)
+                generated_codes.append(code)
+            content += '\n'.join(generated_codes) + '\n'
 
             content += '\nInput(s)\n'
             if stdin['num-input'] >= 1:
-                content += str(deadended.posix.dumps(0)) + '\n'
-                content += str(deadended.posix.dumps(1)) + '\n'
-                content += str(deadended.posix.dumps(2)) + '\n'
-                print('input: ', deadended.posix.dumps(0))
-                print('input: ', deadended.posix.dumps(1))
-                print('input: ', deadended.posix.dumps(2))
+                dump_0 = str(deadended.posix.dumps(0))
+                print('input: ', dump_0)
+                generated_passwords.append(dump_0)
+                dump_1 = str(deadended.posix.dumps(1))
+                print('input: ', dump_1)
+                generated_passwords.append(dump_1)
+                dump_2 = str(deadended.posix.dumps(2))
+                print('input: ', dump_2)
+                generated_passwords.append(dump_2)
                 print()
+            content += '\n'.join(generated_passwords) + '\n'
 
             with open(output_test_file_path, 'w') as test_file:
                 test_file.write(content)
@@ -59,7 +70,11 @@ def symbolic_execution(input_file_path, output_dir_path, stdin):
     time_taken = end_time - start_time
 
     return {
-        'time': time_taken.total_seconds()
+        'generated-codes': generated_codes,
+        'time-taken': time_taken.total_seconds(),
+        'generated-passwords': generated_passwords,
+        'is-code-cracked': lists.has_string(credentials['code'], generated_codes),
+        'is-password-cracked': lists.has_string(credentials['password'], generated_passwords),
     }
 
 
@@ -69,10 +84,10 @@ def compile(input_path, output_path):
     os.system(cmd)
 
 
-def run(input_file_path, output_dir_path, stdin):
+def run(input_file_path, output_dir_path, stdin, options, credentials):
     input_file = file.details(input_file_path)
     output_file = FILE_NAME['c-out'].format(name = input_file['name'])
     output_file_path = os.path.join(output_dir_path, output_file)
     compile(input_file_path, output_file_path)
 
-    return symbolic_execution(output_file_path, output_dir_path, stdin)
+    return symbolic_execution(output_file_path, output_dir_path, stdin, options, credentials)
