85a215c602d1835f8f5c7ba8638e379b16351435
Deepak Adhikari <deeps.adhi@gmail.com>
2019-07-03 09:48:03 -0500

    Add FILE_NAME constants

--- core/const.py
+++ core/const.py
@@ -1,29 +1,34 @@
-CMD = {
-    'bash': '/bin/bash -c "{}"',
-    'gcc': 'gcc {input} -o {output}'
-}
-
-DIR_NAME = {
-    'samples': 'samples'
-}
-
-FILE_NAME = {
-    'empty-c-file': 'empty.c',
-    'analyis': 'analysis.csv',
-}
-
 SE = 'se'
-C_EXT = '.'
+ONE_BYTE = 8
+C_EXT = '.c'
 ANGR = 'angr'
 KLEE = 'klee'
 GENERATE = 'generate'
 OBFUSCATE ='obfuscate'
 SYMBOLIC_EXECUTION = 'symbolic-execution'
 
+CMD = {
+    'bash': '/bin/bash -c "{}"',
+    'gcc': 'gcc {input} -o {output}',
+}
+
+DIR_NAME = {
+    'samples': 'samples',
+}
+
+FILE_NAME = {
+    'c-out': '{name}.out',
+    'log': '{name}_log.txt',
+    'bytecode': '{name}.bc',
+    'empty-c-file': 'empty.c',
+    'analysis': 'analysis.csv',
+    'angr-test': 'test_angr_{index}.txt',
+}
+
 CSV_HEAD = {
     'file-path': 'Path',
     'file-name': 'File',
     'klee-time': 'Time taken by Klee',
     'angr-time': 'Time taken by Angr',
-    'file-sze': 'File size (in bytes)',
+    'file-size': 'File size (in bytes)',
 }
