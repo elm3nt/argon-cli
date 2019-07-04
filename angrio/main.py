40ac87c82ea5eb11a51870cc7dfcbb2cb2e16948
Deepak Adhikari <deeps.adhi@gmail.com>
2019-07-03 23:37:08 -0500

    Verify credentials

--- angrio/main.py
+++ angrio/main.py
@@ -8,73 +8,87 @@ from utils import lists
 from core.const import *
 
 
-def symbolic_execution(input_file_path, output_dir_path, stdin, options, credentials):
-    start_time = datetime.now()
-
-    project = angr.Project(input_file_path)
-
+def init_project(input_file_path, stdin, project):
     args = []
+    state = project.factory.entry_state()
+
     if stdin['num-arg'] >= 1:
         for i in range(0, stdin['num-arg']):
             args.append(claripy.BVS('arg'+str(i), int(stdin['length-arg']) * ONE_BYTE))
 
         entry_state = [input_file_path] + args
         state = project.factory.entry_state(args = entry_state)
-    else:
-        state = project.factory.entry_state()
 
-    simulation_manager = project.factory.simulation_manager(state)
+    return {
+        'args': args,
+        'state': state,
+    }
+
+
+def decode_credentials(output_dir_path, stdin, args, simulation_manager):
+    index = 0
+    codes = []
+    passwords = []
+
+    for deadended in simulation_manager.deadended:
+        content = ''
+        test_file_name = FILE_NAME['angr-test'].format(index = str(index))
+        output_test_file_path = os.path.join(output_dir_path, test_file_name)
+
+        content += 'Arg(s)\n'
+        for arg in args:
+            code = str(deadended.solver.eval(arg, cast_to = bytes))
+            codes.append(code)
+        content += lists.to_str_with_nl(codes) + '\n'
+
+        content += '\nInput(s)\n'
+        if stdin['num-input'] >= 1:
+            dump_0 = str(deadended.posix.dumps(0))
+            print('input: ', dump_0)
+            passwords.append(dump_0)
+            dump_1 = str(deadended.posix.dumps(1))
+            print('input: ', dump_1)
+            passwords.append(dump_1)
+            dump_2 = str(deadended.posix.dumps(2))
+            print('input: ', dump_2)
+            passwords.append(dump_2)
+            print()
+        content += lists.to_str_with_nl(passwords) + '\n'
+
+        file.write(output_test_file_path, content)
+
+        index += 1
+
+    return {
+        'codes': codes,
+        'passwords': passwords,
+    }
+
+
+def symbolic_execution(input_file_path, output_dir_path, stdin, options, credentials):
+    start_time = datetime.now()
+
+    project = angr.Project(input_file_path)
+    project_status = init_project(input_file_path, stdin, project)
+    simulation_manager = project.factory.simulation_manager(project_status['state'])
     simulation_manager.run()
 
     end_time = datetime.now()
-
-    if hasattr(simulation_manager, 'deadended'):
-        index = 0
-
-        codes = ''
-        passwords = ''
-        generated_codes = []
-        generated_passwords = []
-
-        for deadended in simulation_manager.deadended:
-            content = ''
-            test_file_name = FILE_NAME['angr-test'].format(index = str(index))
-            output_test_file_path = os.path.join(output_dir_path, test_file_name)
-
-            content += 'Arg(s)\n'
-            for arg in args:
-                code = str(deadended.solver.eval(arg, cast_to = bytes))
-                print('arg: ', code)
-                generated_codes.append(code)
-            content += '\n'.join(generated_codes) + '\n'
-
-            content += '\nInput(s)\n'
-            if stdin['num-input'] >= 1:
-                dump_0 = str(deadended.posix.dumps(0))
-                print('input: ', dump_0)
-                generated_passwords.append(dump_0)
-                dump_1 = str(deadended.posix.dumps(1))
-                print('input: ', dump_1)
-                generated_passwords.append(dump_1)
-                dump_2 = str(deadended.posix.dumps(2))
-                print('input: ', dump_2)
-                generated_passwords.append(dump_2)
-                print()
-            content += '\n'.join(generated_passwords) + '\n'
-
-            with open(output_test_file_path, 'w') as test_file:
-                test_file.write(content)
-
-            index += 1
-
     time_taken = end_time - start_time
 
+    generated = {
+        'codes': [],
+        'passwords': [],
+    }
+    if hasattr(simulation_manager, 'deadended'):
+        generated = decode_credentials(output_dir_path, stdin, project_status['args'], simulation_manager)
+
     return {
-        'generated-codes': generated_codes,
         'time-taken': time_taken.total_seconds(),
-        'generated-passwords': generated_passwords,
-        'is-code-cracked': lists.has_string(credentials['code'], generated_codes),
-        'is-password-cracked': lists.has_string(credentials['password'], generated_passwords),
+        'generated-codes': lists.to_str_with_nl(generated['codes']),
+        'generated-passwords': lists.to_str_with_nl(generated['passwords']),
+        'is-code-cracked': lists.has_string(credentials['codes'], generated['codes']),
+        'is-password-cracked': lists.has_string(credentials['passwords'], generated['passwords']),
     }
 
 
