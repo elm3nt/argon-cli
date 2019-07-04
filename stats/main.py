67356c90b125c565bd62485342028bc49eadf068
Deepak Adhikari <deeps.adhi@gmail.com>
2019-07-03 23:41:12 -0500

    Add CSV header names

--- stats/main.py
+++ stats/main.py
@@ -1,7 +1,36 @@
 import csv
 
+from core.const import *
+from .const import CSV_HEADER as HEAD
 
-def analysis(output_file_path, data):
-    with open(output_file_path, 'w') as analysis_file:
-        writer = csv.writer(analysis_file)
+
+def write_to_file(output_file_path, data):
+    with open(output_file_path, 'w') as file:
+        writer = csv.writer(file)
         writer.writerows(data)
+
+
+def get_csv_header(tool):
+    left = [ HEAD['file-name'], HEAD['file-size'] ]
+    middle = []
+    right = [ HEAD['file-path'] ]
+
+    if tool == ANGR:
+        middle = [ HEAD['angr-time'], HEAD['angr-is-code-cracked'], HEAD['angr-is-password-cracked'],
+                   HEAD['angr-generated-codes'], HEAD['angr-generated-passwords'] ]
+
+    elif tool == KLEE:
+        middle = [ HEAD['klee-time'], HEAD['klee-is-code-cracked'], HEAD['klee-is-password-cracked'],
+                   HEAD['klee-generated-codes'], HEAD['klee-generated-passwords'] ]
+
+    elif tool == EXECUTION:
+        middle = [ HEAD['execution-time'] ]
+
+    elif tool == ALL:
+        middle = [ HEAD['execution-time'], HEAD['angr-time'], HEAD['klee-time'], HEAD['angr-is-code-cracked'],
+                   HEAD['klee-is-code-cracked'], HEAD['angr-is-password-cracked'], HEAD['klee-is-password-cracked'],
+                   HEAD['angr-generated-codes'], HEAD['klee-generated-codes'], HEAD['angr-generated-passwords'],
+                   HEAD['klee-generated-passwords'] ]
+
+    return left + middle + right
+
