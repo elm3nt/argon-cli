4f847613003aa09d83aef409683fddd7388260a7
Deepak Adhikari <deeps.adhi@gmail.com>
2019-07-03 23:44:21 -0500

    Add to_str_with_nl function

--- utils/lists.py
+++ utils/lists.py
@@ -1,10 +1,13 @@
 import re
 
 
-def has_string(arrs1, arrs2):
-    for arr1 in arrs1:
-        has_string = re.search(arr1, ''.join(arrs2))
-        if has_string:
+def has_string(pattern_list, content_list):
+    for pattern in pattern_list:
+        if pattern in ''.join(content_list):
             return True
 
     return False
+
+
+def to_str_with_nl(array):
+    return '\n'.join(array)
