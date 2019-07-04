e0de081142767590767d713b39672996e27566b4
Deepak Adhikari <deeps.adhi@gmail.com>
2019-07-04 03:00:18 -0500

    Fix get_time function

--- utils/time.py
+++ utils/time.py
@@ -1,22 +1,22 @@
+import re
+
+
 def get_time(content):
-    hour =  re.search(r'real\t(.*?)h(.*?)m(.*?)s', content)
-    if hour:
-        hh = int(hour.group(1))
+    hr_min_sec =  re.search(r'real[\s]*(.*?)h(.*?)m(.*?)s', content)
+    if hr_min_sec:
+        hh = int(hr_min_sec.group(1))
+        mm = int(hr_min_sec.group(2))
+        ss = int(hr_min_sec.group(3))
+        ms = int(hr_min_sec.group(4))
 
-        min_sec = re.search(r'real\t[\d]*h(.*?)m(.*?)s', content)
-        if min_sec:
-            mm = int(min_sec.group(1))
-            ss = int(min_sec.group(2).split('.')[0])
-            ms = int(min_sec.group(2).split('.')[1])
-
-            return (hh * 3600) + (mm * 60) + ss + (ms / 1000)
+        return (hh * 3600) + (mm * 60) + ss + (ms / 1000)
 
     else:
-        min_sec = re.search(r'real\t(.*?)m(.*?)s', content)
+        min_sec = re.search(r'real[\s]*(.*?)m(.*?)\.(.*?)s', content)
         if min_sec:
             mm = int(min_sec.group(1))
-            ss = int(min_sec.group(2).split('.')[0])
-            ms = int(min_sec.group(2).split('.')[1])
+            ss = int(min_sec.group(2))
+            ms = int(min_sec.group(3))
 
             return (mm * 60) + ss + (ms / 1000)
 
