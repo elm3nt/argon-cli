001bf30821b0f806d90dfd12bb0e0c1aee17e6a3
Deepak Adhikari <deeps.adhi@gmail.com>
2019-07-04 02:59:39 -0500

    Add function to read file

--- utils/file.py
+++ utils/file.py
@@ -16,6 +16,14 @@ def details(file_path):
     }
 
 
+def read(path):
+    content = ''
+    with open(path, 'r') as file:
+        content = file.read()
+
+    return content
+
+
 def write(path, content):
     with open(path, 'w') as file:
         file.write(content)
